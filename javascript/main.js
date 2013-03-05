e3992de6826a9bec2f73d93eb7d8bd1ffb5ff379
floatec <mp@floatec.de>
2013-03-05 17:10:42 +0100

    marge

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,7 @@
 var gamejs = require('gamejs');
 var view = require('./view');
 
-gamejs.preload(['./data/tiles.png', './data/player.png', './data/hero.png']);
+gamejs.preload(['./data/tiles.png', './data/player.png', './data/hero.png','./data/splash.png']);
 
 var SCREEN_WIDTH = 800;
 var SCREEN_HEIGHT = 480;
@@ -9,6 +9,32 @@ var GRAVITY = 4;
 var JUMP_LENGHT = 0.3;
 
 var map;
+
+function SplashScreen() {
+    this.showSplash=true;
+    SplashScreen.superConstructor.apply(this, arguments);
+
+    //Load and flip image
+    this.image = gamejs.image.load("./data/splash.png");
+
+    //Generate random position at top of screen
+    this.size = this.image.getSize();
+    this.rect = new gamejs.Rect([0,0], this.size);
+
+
+    this.update = function(dt) {
+
+
+    };
+    this.handle = function(event) {
+
+        if (event.type === gamejs.event.MOUSE_DOWN) {
+            this.showSplash=false;
+        }
+    };
+};
+gamejs.utils.objects.extend(SplashScreen, gamejs.sprite.Sprite);
+
 
 function Player(position) {
 	Player.superConstructor.apply(this, arguments);
@@ -76,6 +102,7 @@ function main() {
 	//Initialize variables
 	map = new view.Map('./data/testlevel.tmx');
 	var player = new Player([96, 48]);
+    var splashScreen = new SplashScreen();
 	
 	//The gameloop
 	function gameTick(gameTime) {
@@ -93,6 +120,7 @@ function main() {
 		gamejs.event.get().forEach(function(event) {
 			player.handle(event);
 			map.handle(event);
+            splashScreen.handle(event);
 		});
 		
 		//Update world
@@ -104,10 +132,14 @@ function main() {
 		
 		//Clear background
 		display.clear();
-		
+		if(splashScreen.showSplash){
+            splashScreen.draw(display);
+        }else{
 		//Draw world
 		player.draw(display);
 		map.draw(display);
+        }
+
 	}
 };
 
